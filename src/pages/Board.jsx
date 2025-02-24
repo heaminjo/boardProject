f40a820005ce82ceb912582c4a00dbdd5efe76da
heaminjo <157016835+heaminjo@users.noreply.github.com>
2025-02-24 12:37:38 +0900

    검색 기능 추가
    검색어를 입력 받아 filter 함수를 사용하여 걸려낸 배열을 출력
    검색어가 없다면 기존의 리스트를 가져와서 출력

--- src/pages/Board.jsx
+++ src/pages/Board.jsx
@@ -1,9 +1,10 @@
 import { Link, NavLink, Route, Routes, useNavigate } from "react-router-dom";
 import Post from "./Post";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import BoardComp from "./BoardStyle";
+import BoardList from "../components/board/BoardList";
 
-const Data = [
+const data = [
   { id: "1", title: "개구리와 왕자", content: "개구리가 여행하는 내용." },
   { id: "2", title: "신데렐라", content: "유리 구두 찾는 내용." },
   { id: "3", title: "백설공주", content: "사과 먹는 내용." },
@@ -12,37 +13,43 @@ const Data = [
 ];
 
 export default function Board() {
-  //게시판 데이터를 순회 메서드를 사용해 배열에 담아준다.
-  const boardList = Data.map(({ id, title, content }) => {
-    console.log(id);
-    return (
-      <li>
-        {/* 게시글 상세 조회를 위해 id로 경로 설정 */}
-        <NavLink to={`/board/post/${id}`}>
-          {id} {title} - {content}
-        </NavLink>
-      </li>
-    );
-  });
+  const [list, setList] = useState(data); //board 리스트
 
-  // const onClickPost = (id){
-  //   localStorage
-  // }
+  const [search, setSearch] = useState("");
 
-  //페이지가 로드 되면 게시물 데이터를 localStorage에 저장
-  useEffect(() => {
-    Data.map((m) => {
-      localStorage.setItem(m.id, JSON.stringify(m));
-    });
-  }, []);
+  //게시글 검색
+  const boardSearch = () => {
+    //검색어를 채웠을 경우
+    if (search !== "") {
+      const newList = list.filter(({ content }) =>
+        content.toLowerCase().includes(search.toLowerCase())
+      );
+      setList(newList);
+    } else setList(data);
+  };
 
   return (
     <BoardComp>
       <div className="title">
         <h2>게시판 입니다.</h2>
       </div>
+
+      {/* 리스트 출력 컴포넌트 */}
       <div className="boardList">
-        <ul>{boardList}</ul>
+        <BoardList list={list} />
+      </div>
+
+      {/* new 기능 추가 */}
+      {/* 게시물 검색 */}
+      <div className="boardSearch">
+        <input
+          value={search}
+          onChange={(e) => setSearch(e.target.value)}
+          type="text"
+          placeholder="검색할 게시물을 입력해주세요."
+          // onKeyDown={(e) => onkeydownSearch(e)}
+        />
+        <button onClick={() => boardSearch()}>검색</button>
       </div>
     </BoardComp>
   );
