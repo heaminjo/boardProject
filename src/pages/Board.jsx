9c7ad93d71cc83bec1cd556eabb0d3ba6c33aa5c
heaminjo <157016835+heaminjo@users.noreply.github.com>
2025-02-24 22:58:17 +0900

    게시물 등록 기능 추가
    BoardWrite 컴포넌트 생성 
    Board컴포에서 콜백함수를 전달하여 BoardWrite에서 제목과 내용을 입력받아 콜백함수 호출

--- src/pages/Board.jsx
+++ src/pages/Board.jsx
@@ -1,6 +1,6 @@
 import { Link, NavLink, Route, Routes, useNavigate } from "react-router-dom";
 import Post from "./Post";
-import { useEffect, useState } from "react";
+import React, { useContext, useEffect, useRef, useState } from "react";
 import BoardComp from "./BoardStyle";
 import BoardList from "../components/board/BoardList";
 
@@ -12,12 +12,20 @@ const data = [
   { id: "5", title: "라푼젤젤", content: "머리긴 공주 내용" },
 ];
 
+export const BoardContext = React.createContext();
+
 export default function Board() {
+  const navigate = useNavigate();
+
+  //board의 id 값으로 넣어줄줄 ref
+  const idRef = useRef(data.length);
+
+  //*** 상태변수 ***
   const [list, setList] = useState(data); //원본 board 리스트
   const [filterList, setFilterList] = useState([]); //검색 board 리스트
   const [search, setSearch] = useState("");
 
-  //게시글 검색
+  //*** 게시글 검색 함수 ***
   const boardSearch = () => {
     //검색어를 채웠을 경우
     if (search !== "") {
@@ -29,6 +37,14 @@ export default function Board() {
     } else setFilterList([]);
   };
 
+  // *** 게시글 추가 함수 ***
+  const addBoard = ({ title, content }) => {
+    setList({ id: idRef, title: title, content: content });
+    idRef++;
+    console.log("추가 완료");
+  };
+
+  //--------------------------------
   return (
     <BoardComp>
       <div className="title">
@@ -46,15 +62,24 @@ export default function Board() {
 
       {/* new 기능 추가 */}
       {/* 게시물 검색 */}
-      <div className="boardSearch">
-        <input
-          value={search}
-          onChange={(e) => setSearch(e.target.value)}
-          type="text"
-          placeholder="검색할 게시물을 입력해주세요."
-          // onKeyDown={(e) => onkeydownSearch(e)}
-        />
-        <button onClick={() => boardSearch()}>검색</button>
+
+      <div className="boardService">
+        <div className="boardWrite">
+          {/* 에러********************* */}
+          <BoardContext.Provider value={addBoard}>
+            <button onClick={() => navigate("/write")}>게시물 작성</button>
+          </BoardContext.Provider>
+        </div>
+        <div className="boardSearch">
+          <input
+            value={search}
+            onChange={(e) => setSearch(e.target.value)}
+            type="text"
+            placeholder="검색할 게시물을 입력해주세요."
+            // onKeyDown={(e) => onkeydownSearch(e)}
+          />
+          <button onClick={() => boardSearch()}>검색</button>
+        </div>
       </div>
     </BoardComp>
   );
